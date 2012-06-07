93c4f975943d24d11836b0d0da1ff0fcc49ca90b
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-06-06 18:35:44 -0700

    Just sync the whole thing in one batch.
    This is useful for two reasons:
    
     - sync problem is self-correcting. if one version failed to sync at one
       point, it'll eventually get synced.
    
     - we stopped serving binaries from mirror.jenkins-ci.org all together,
       and require files to be on mirrors. So files on mirror that's not
       pushed to OSUOSL is pointless, as they only cause 404.
    
    If the footprint of old files are of concern, we should probably push
    them into a dedicated 'archives' directory or something that't not
    pushed to OSUOSL (and for those we can serve from cucumber.)

--- run.sh
+++ run.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
-echo ">> Mirroring all of 1.${Version}"
-
 HOST=osuosl
 BASE_DIR=/srv/releases/hudson
 REMOTE_BASE_DIR=data/
 
-FILE=${BASE_DIR}/windows${ReleaseLine}/jenkins-1.${Version}.zip
-echo ">> Mirroring ${FILE}"
-rsync -avz $FILE $HOST:jenkins/windows${ReleaseLine}/
-
-FILE=${BASE_DIR}/osx${ReleaseLine}/jenkins-1.${Version}.pkg
-echo ">> Mirroring ${FILE}"
-rsync -avz $FILE $HOST:jenkins/osx${ReleaseLine}/
-
-FILE=${BASE_DIR}/debian${ReleaseLine}/jenkins_1.${Version}_all.deb
-echo ">> Mirroring ${FILE}"
-rsync -avz $FILE $HOST:jenkins/debian${ReleaseLine}/
-
-FILE=${BASE_DIR}/opensuse${ReleaseLine}/jenkins-1.${Version}*-*.noarch.rpm
-echo ">> Mirroring ${FILE}"
-rsync -avz  $FILE $HOST:jenkins/opensuse${ReleaseLine}/
-
-FILE=${BASE_DIR}/redhat${ReleaseLine}/jenkins-1.${Version}*-*.noarch.rpm
-echo ">> Mirroring ${FILE}"
-rsync -avz  $FILE $HOST:jenkins/redhat${ReleaseLine}/
-
-FILE=${BASE_DIR}/war${ReleaseLine}/1.${Version}
-echo ">> Mirroring ${FILE}"
-rsync -avz -r $FILE $HOST:jenkins/war${ReleaseLine}/
+rsync -avz --filter='. rsync.filter' $BASE_DIR/ $HOST:jenkins/
 
 echo ">> Updating the latest symlink"
 ssh www-data@localhost /srv/releases/update-latest-symlink.sh 1.${Version} "${ReleaseLine}"
