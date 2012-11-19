3be3323faeb560a55e5a1964c0a5b7583bd569ec
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-11-18 21:34:57 -0800

    Got rid of the version
    The script now figures out the latest version by itself.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ REMOTE_BASE_DIR=data/
 rsync -avz --filter='. rsync.filter' $BASE_DIR/ $HOST:jenkins/
 
 echo ">> Updating the latest symlink"
-ssh www-data@localhost /srv/releases/update-latest-symlink.sh 1.${Version} "${ReleaseLine}"
+ssh www-data@localhost /srv/releases/update-latest-symlink.sh "${ReleaseLine}"
 
 echo "Triggering remote mirroring script"
 ssh $HOST "sh trigger-hudson"
