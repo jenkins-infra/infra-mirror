e327dbf8e6f010aaed4283b63e85ae24230d584a
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-10-07 16:55:34 -0700

    Fix the time lag during releases between index update to package update
    During the release proess, debian/rpm index files on pkg.jenkins-ci.org
    gets updated before actual package files appear in the mirrors.
    
    This causes 404 until the mirroring kicks in, which can be several hours
    in the bad case.
    
    The release process is now modified to update index in
    /var/www/pkg.jenkins-ci.org.staging. This change synchronizes the
    delivery of bits to mirrors (at least to the fallback mirror) and
    the updates to the repo index

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@ ssh www-data@localhost /srv/releases/populate-fallback.sh
 echo ">> Updating the latest symlink"
 ssh www-data@localhost /srv/releases/update-latest-symlink.sh "${ReleaseLine}"
 
-echo "Triggering remote mirroring script"
+echo ">> Triggering remote mirroring script"
 ssh $HOST "sh trigger-hudson"
 
+echo ">> move index from staging to production"
+ssh www-data@localhost "cd /var/www; rsync -avz pkg.jenkins-ci.org.staging/ pkg.jenkins-ci.org/"
